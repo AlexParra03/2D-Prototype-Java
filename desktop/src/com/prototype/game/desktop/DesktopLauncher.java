b55157d3234cb3330227052a06c950e267416421
Alex Parra <Alex Parra>
2017-10-07 15:28:31 -0400

    project setup

---
+++ desktop/src/com/prototype/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.prototype.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.prototype.game.MainPrototype;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainPrototype(), config);
+	}
+}
