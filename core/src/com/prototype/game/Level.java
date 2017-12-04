9bae80a6590d982e0938e73d62a019c4f3627a81
GIAN1904 <32659736+GIAN1904@users.noreply.github.com>
2017-12-04 00:41:16 -0500

    added levels
    testing level.java

--- core/src/com/prototype/game/Level.java
+++ core/src/com/prototype/game/Level.java
@@ -57,6 +57,17 @@ public class Level implements RenderableObject {
                 case 4:
                     buildLevelFour();
                     break;
+                case 5:
+                	buildlevelFive();
+                	break;
+                case 6:
+                	buildlevelSix();
+                	break;
+                case 7:
+                	buildlevelSeven();
+                	break;
+                
+                	
             }
             
             // ---------------------------------
@@ -64,8 +75,8 @@ public class Level implements RenderableObject {
             this.player.setGameObjects(this.objects);
         }
     }
-    
-    public void buildLevelFour(){
+    //new level 7
+    public void buildlevelSeven(){
         this.map = new TileMap(30,20);
         this.objects = new Array<GameObject>();
         
@@ -73,7 +84,7 @@ public class Level implements RenderableObject {
         Callback functionOne = new Callback(){
             @Override
             public void action(Level level) {
-                level.dialog.show("All the things");
+                level.dialog.show("Welcome to level 7");
                 
             }
             
@@ -82,7 +93,7 @@ public class Level implements RenderableObject {
         Callback functionTwo = new Callback(){
             @Override
             public void action(Level level) {
-                level.input.show("Convert 11 to Decimal");
+                level.input.show("Convert (111111) 2   to decimal");
                 level.dialog.show("When you are done check with the other computer");
                 
             }
@@ -93,8 +104,8 @@ public class Level implements RenderableObject {
             @Override
             public void action(Level level) {
                 String inputFromText = level.input.text;
-                if(inputFromText.equals("3")){
-                    level.objects.add( factory.create("key", 300, 130) );
+                if(inputFromText.equals("63")){
+                    level.objects.add( factory.create("key", 700, 130) );
                 }
                 
             }
@@ -110,7 +121,240 @@ public class Level implements RenderableObject {
             }
             
         };
+        objects.add( factory.create("rock", 100, 90)  );
+        objects.add( factory.create("rock", 120, 90)  );
+        objects.add( factory.create("rock", 140, 90)  );
+        objects.add( factory.create("rock", 160, 90)  );
+        objects.add( factory.create("rock", 180, 90)  );
+        objects.add( factory.create("rock", 200, 90)  );
+        objects.add( factory.create("rock", 220, 90)  );
+        objects.add( factory.create("rock", 240, 90)  );
+        objects.add( factory.create("rock", 260, 90)  );
+        objects.add( factory.create("rock", 280, 90)  );
+        objects.add( factory.create("rock", 300, 90)  );
+        objects.add( factory.create("rock", 320, 90)  );
+        objects.add( factory.create("rock", 340, 90)  );
+        objects.add( factory.create("rock", 360, 90)  );
+        objects.add( factory.create("rock", 380, 90)  );
+        objects.add( factory.create("rock", 400, 90)  );
+        objects.add( factory.create("rock", 420, 90)  );
+        objects.add( factory.create("rock", 440, 90)  );
+        objects.add( factory.create("rock", 460, 90)  );
+        objects.add( factory.create("rock", 480, 90)  );
+        objects.add( factory.create("rock", 500, 90)  );
+        objects.add( factory.create("barrel", 90, 90 )  );
+        objects.add( factory.create("barrel", 520, 90)  );
+        objects.add( factory.create("rock", 100, 400)  );
+        objects.add( factory.create("rock", 120, 400)  );
+        objects.add( factory.create("rock", 140, 400)  );
+        objects.add( factory.create("rock", 160, 400)  );
+        objects.add( factory.create("rock", 180, 400)  );
+        objects.add( factory.create("rock", 200, 400)  );
+        objects.add( factory.create("rock", 220, 400)  );
+        objects.add( factory.create("rock", 240, 400)  );
+        objects.add( factory.create("rock", 260, 400)  );
+        objects.add( factory.create("rock", 280, 400)  );
+        objects.add( factory.create("rock", 300, 400)  );
+        objects.add( factory.create("rock", 320, 400)  );
+        objects.add( factory.create("rock", 340, 400)  );
+        objects.add( factory.create("rock", 360, 400)  );
+        objects.add( factory.create("rock", 380, 400)  );
+        objects.add( factory.create("rock", 400, 400)  );
+        objects.add( factory.create("rock", 420, 400)  );
+        objects.add( factory.create("rock", 440, 400)  );
+        objects.add( factory.create("rock", 460, 400)  );
+        objects.add( factory.create("rock", 480, 400)  );
+        objects.add( factory.create("rock", 500, 400)  );
+        objects.add( factory.create("barrel", 90, 600, functionOne)  );
+        objects.add( factory.create("barrel", 520, 600)  );
+
         
+        objects.add( factory.create("computer", 600, 200, functionTwo)  );
+        
+        objects.add( factory.create("computer", 400, 200, functionThree)  );
+        
+        
+        objects.add( factory.createDoor("up", 100, 300, teleport, 1 ) );
+    }
+    // new level 6
+    public void buildlevelSix(){
+        this.map = new TileMap(30,20);
+        this.objects = new Array<GameObject>();
+        
+        
+        Callback functionOne = new Callback(){
+            @Override
+            public void action(Level level) {
+                level.dialog.show("Welcome to level 6");
+                
+            }
+            
+        };
+        
+        Callback functionTwo = new Callback(){
+            @Override
+            public void action(Level level) {
+                level.input.show("Convert (11001011) 2  to decimal");
+                level.dialog.show("When you are done check with the other computer");
+                
+            }
+            
+        };
+        
+        Callback functionThree = new Callback(){
+            @Override
+            public void action(Level level) {
+                String inputFromText = level.input.text;
+                if(inputFromText.equals("203")){
+                    level.objects.add( factory.create("key", 700, 130) );
+                }
+                
+            }
+            
+        };
+        
+                 
+                
+        objects.add( factory.create("rock", 100, 400)  );
+        objects.add( factory.create("rock", 120, 400)  );
+        objects.add( factory.create("rock", 140, 400)  );
+        objects.add( factory.create("rock", 160, 400)  );
+        objects.add( factory.create("rock", 180, 400)  );
+        objects.add( factory.create("rock", 200, 400)  );
+        objects.add( factory.create("rock", 220, 400)  );
+        objects.add( factory.create("rock", 240, 400)  );
+        objects.add( factory.create("rock", 260, 400)  );
+        objects.add( factory.create("rock", 280, 400)  );
+        objects.add( factory.create("rock", 300, 400)  );
+        objects.add( factory.create("rock", 320, 400)  );
+        objects.add( factory.create("rock", 340, 400)  );
+        objects.add( factory.create("rock", 360, 400)  );
+        objects.add( factory.create("rock", 380, 400)  );
+        objects.add( factory.create("rock", 400, 400)  );
+        objects.add( factory.create("rock", 420, 400)  );
+        objects.add( factory.create("rock", 440, 400)  );
+        objects.add( factory.create("rock", 460, 400)  );
+        objects.add( factory.create("rock", 480, 400)  );
+        objects.add( factory.create("rock", 500, 400)  );
+        objects.add( factory.create("barrel", 90, 400, functionOne)  );
+        objects.add( factory.create("barrel", 520, 400)  );
+
+        
+        objects.add( factory.create("computer", 600, 200, functionTwo)  );
+        
+        objects.add( factory.create("computer", 400, 200, functionThree)  );
+        
+        
+        objects.add( factory.createDoor("up", 100, 300, 7 ) );
+    }
+    // new level 5
+    public void buildlevelFive(){
+        this.map = new TileMap(30,20);
+        this.objects = new Array<GameObject>();
+        
+        
+        Callback functionOne = new Callback(){
+            @Override
+            public void action(Level level) {
+                level.dialog.show("Welcome to level 5");
+                
+            }
+            
+        };
+        
+        Callback functionTwo = new Callback(){
+            @Override
+            public void action(Level level) {
+                level.input.show("Convert (1 0101 1111)2 to decimal");
+                level.dialog.show("When you are done check with the other computer");
+                
+            }
+            
+        };
+        
+        Callback functionThree = new Callback(){
+            @Override
+            public void action(Level level) {
+                String inputFromText = level.input.text;
+                if(inputFromText.equals("351")){
+                    level.objects.add( factory.create("key", 700, 130) );
+                }
+                
+            }
+            
+        };
+        
+              
+        objects.add( factory.create("rock", 100, 90)  );
+        objects.add( factory.create("rock", 120, 90)  );
+        objects.add( factory.create("rock", 140, 90)  );
+        objects.add( factory.create("rock", 160, 90)  );
+        objects.add( factory.create("rock", 180, 90)  );
+        objects.add( factory.create("rock", 200, 90)  );
+        objects.add( factory.create("rock", 220, 90)  );
+        objects.add( factory.create("rock", 240, 90)  );
+        objects.add( factory.create("rock", 260, 90)  );
+        objects.add( factory.create("rock", 280, 90)  );
+        objects.add( factory.create("rock", 300, 90)  );
+        objects.add( factory.create("rock", 320, 90)  );
+        objects.add( factory.create("rock", 340, 90)  );
+        objects.add( factory.create("rock", 360, 90)  );
+        objects.add( factory.create("rock", 380, 90)  );
+        objects.add( factory.create("rock", 400, 90)  );
+        objects.add( factory.create("rock", 420, 90)  );
+        objects.add( factory.create("rock", 440, 90)  );
+        objects.add( factory.create("rock", 460, 90)  );
+        objects.add( factory.create("rock", 480, 90)  );
+        objects.add( factory.create("rock", 500, 90)  );
+        objects.add( factory.create("barrel", 90, 90, functionOne)  );
+        objects.add( factory.create("barrel", 520, 90)  );
+
+        
+        objects.add( factory.create("computer", 600, 200, functionTwo)  );
+        
+        objects.add( factory.create("computer", 400, 200, functionThree)  );
+        
+        
+        objects.add( factory.createDoor("up", 100, 300, 6 ) );
+    }
+    
+    public void buildLevelFour(){
+        this.map = new TileMap(30,20);
+        this.objects = new Array<GameObject>();
+        
+        
+        Callback functionOne = new Callback(){
+            @Override
+            public void action(Level level) {
+                level.dialog.show("Welcome to level four");
+                
+            }
+            
+        };
+        
+        Callback functionTwo = new Callback(){
+            @Override
+            public void action(Level level) {
+                level.input.show("Convert (10010) 2  to Decimal");
+                level.dialog.show("When you are done check with the other computer");
+                
+            }
+            
+        };
+        
+        Callback functionThree = new Callback(){
+            @Override
+            public void action(Level level) {
+                String inputFromText = level.input.text;
+                if(inputFromText.equals("18")){
+                    level.objects.add( factory.create("key", 300, 130) );
+                }
+                
+            }
+            
+        };
+                           
+                
         objects.add( factory.create("rock", 45, 90)  );
         
         objects.add( factory.create("computer", 200, 90, functionTwo)  );
@@ -119,7 +363,7 @@ public class Level implements RenderableObject {
         
         objects.add( factory.create("barrel", 100, 90, functionOne)  );
         
-        objects.add( factory.createDoor("up", 100, 300, teleport, 1 ) );
+        objects.add( factory.createDoor("up", 100, 300,  5 ) );
     }
     
     public void buildLevelThree(){
@@ -134,7 +378,7 @@ public class Level implements RenderableObject {
                 level.player.x = 400;
                 level.player.y = 400;
                 level.map.map[7][7] = 4;
-                level.input.show("Type the answer here?");
+                level.input.show("Convert (1 1011) 2 to decimal");
             }
         };
         
@@ -142,18 +386,18 @@ public class Level implements RenderableObject {
             @Override
             public void action(Level level) {
                 String textFromInput = level.input.text;
-                if(textFromInput.equals("CAT")){
+                if(textFromInput.equals("27")){
                     level.objects.add( level.factory.create("key", 180, 120) );
                 }
                 level.selectLevel(4);
             }
         };
         
-        objects.add( factory.create("computer", 30, 30, function1)  );
-        objects.add( factory.create("box", 100, 100, function2));
+        objects.add( factory.create("computer", 330, 100, function1)  );
+        objects.add( factory.create("box", 430, 100, function2));
         
         
-        objects.add( factory.createDoor("up", 100, 160, 1) );
+        objects.add( factory.createDoor("up", 400, 400, 4) );
         
     }
     
@@ -165,7 +409,7 @@ public class Level implements RenderableObject {
         Callback function1 = new Callback(){
             @Override
             public void action(Level level) {
-               level.input.show("Hello");
+               level.input.show("Hello, Welcome to Binary and Boolean Number Game");
             }
             
         };
@@ -180,7 +424,7 @@ public class Level implements RenderableObject {
             
         };
         
-        this.objects.add(factory.createDoor("up", 400, 400, 3));
+        this.objects.add(factory.createDoor("up", 400, 400, 2));
         this.objects.add(factory.create("computer", 200, 200, function1));
         this.objects.add(factory.create("key", 300, 200));
         this.objects.add(factory.create("key", 500, 300));
@@ -196,7 +440,7 @@ public class Level implements RenderableObject {
         Callback function1 = new Callback(){
             @Override
             public void action(Level level) {
-               level.input.show("Hello");
+               level.input.show("The next level shows answer of binary conversion");
             }
             
         };
@@ -211,10 +455,10 @@ public class Level implements RenderableObject {
             
         };
         
-        this.objects.add(factory.createDoor("up", 500, 400, 1));
-        this.objects.add(factory.create("computer", 250, 200, function1));
-        this.objects.add(factory.create("key", 330, 200));
-        this.objects.add(factory.create("key", 500, 340));
+        this.objects.add(factory.createDoor("up", 500, 400, 3));
+        this.objects.add(factory.create("computer", 300, 100, function1));
+        this.objects.add(factory.create("key", 230, 200));
+        this.objects.add(factory.create("key", 400, 340));
         this.map.map[5][5] = 3;
         this.map.map[6][6] = 3;
         
